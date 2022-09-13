710a5c26a809a5956fe44660675fb0133a99ba97
vararrayobject <vararrayobject@gmail.com>
2022-09-13 23:40:28 +0530

    remove extra files and code

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,3 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', ['uses' => '\App\Http\Controllers\PeopleController@index']);
-Route::get('/people', ['uses' => '\App\Http\Controllers\PeopleController@people']);
